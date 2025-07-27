3d11dec21e709da103aca5a71634531efe8c633a
Paul J <paujo.kld@gmail.com>
2025-07-27 18:24:54 +1000

    src/testpackagepaul/DemoForLogin.java

--- src/testpackagepaul/DemoForLogin.java
+++ src/testpackagepaul/DemoForLogin.java
@@ -1,10 +1,16 @@
 package testpackagepaul;
 
+import java.security.Identity;
+
+import org.xml.sax.Locator;
+
 public class DemoForLogin {
 
 	public static void main(String[] args) {
 		int i= 10;
 		System.out.println("testing");
+		
+		pivate By Locator = By.id("name"));
 
 	}
 
